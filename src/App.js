724fdccd8e5ffcf93cac7b46222a4e378fd11a44
pimentacaroline <pimenta.caroline@gmail.com>
2023-09-25 20:42:12 +0200

    Created unit test features 1, 2, 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-
+      <CitySearch />
+      <EventList />
+      <NumberOfEvents />
     </div>
   );
 }
