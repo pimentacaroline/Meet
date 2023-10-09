d96d492cc33e7f9d04419d0f09bd62f11de835d7
pimentacaroline <pimenta.caroline@gmail.com>
2023-10-09 20:38:33 +0200

    updated nprogress

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
-
-
 import mockData from './mock-data';
+import NProgress from "nprogress";
+import axios from "axios";
 
 /**
  *
@@ -52,14 +52,17 @@ export const extractLocations = (events) => {
 
 export const getEvents = async () => {
 
+  NProgress.start();
+
   if (window.location.href.startsWith('http://localhost')) {
+    NProgress.done();
     return mockData;
   }
 
   if (!navigator.onLine) {
-    const events = localStorage.getItem("lastEvents");
+    const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return events?JSON.parse(events):[];
+    return data ? JSON.parse(data).events : [];
   }
 
   const token = await getAccessToken();
@@ -67,13 +70,14 @@ export const getEvents = async () => {
   if (token) {
     removeQuery();
     const url =  "https://usxf3hdcn1.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      NProgress.done();
-      localStorage.setItem("lastEvents", JSON.stringify(result.events));
-      return result.events;
-    } else return null; 
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
   }
 };
 
